886b263cf2a679d6314ec5d017ad91ff80a43659
AntonioUngolo <antonio.ungolo@outlook.com>
2023-06-22 18:19:36 +0200

    working on calculation

--- script.js
+++ script.js
@@ -6,29 +6,41 @@ const inputHeight = document.getElementById('height');
 const inputWeight = document.getElementById('weight');
 const noResults = document.getElementById('no-results');
 const results = document.getElementById('results');
+let bmi = document.getElementById('bmi');
+
+let heightMetric;
+let weightMetric;
 
 function checkInput() {
   if (inputHeight.value !== '' && inputWeight.value !== '') {
-    console.log('Inputs contains values:', inputHeight.value, inputWeight.value);
+    // console.log('Inputs contains values:', inputHeight.value, inputWeight.value);
     results.classList.remove('hidden');
     results.classList.add('flex');
     noResults.classList.add('hidden');
+    calcBmiMetric();
   } else {
-    console.log('Input is empty.');
+    // console.log('Input is empty.');
     noResults.classList.remove('hidden');
     results.classList.add('hidden');
     results.classList.remove('flex');
   }
 }
 
-checkInput();
+function calcBmiMetric() {
+  heightMetric = heightMetric / 100;
+  const BMI = weightMetric / (heightMetric * heightMetric);
+  console.log(BMI.toFixed(2));
+  return (bmi.textContent = BMI.toFixed(2));
+}
 
-inputHeight.addEventListener('input', function (event) {
-  console.log(event.target.value);
+inputHeight.addEventListener('blur', function (event) {
+  // console.log(event.target.value);
+  heightMetric = event.target.value;
   checkInput();
 });
 
-inputWeight.addEventListener('input', function (event) {
-  console.log(event.target.value);
+inputWeight.addEventListener('blur', function (event) {
+  // console.log(event.target.value);
+  weightMetric = event.target.value;
   checkInput();
 });
